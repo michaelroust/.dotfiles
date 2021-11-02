862ef53ef0134f9f952479884489730e1db4ad11
metaTinker <michael.roust@gmail.com>
2021-11-02 13:00:31 +0100

    m

--- setup.zsh
+++ setup.zsh
@@ -4,9 +4,9 @@ cd ~
 ln -s "/Users/mroust/Library/Mobile Documents/com~apple~CloudDocs/code" code
 
 # Symlink each .dotfiles
-ln -s code/.dotfiles/.bash_profile .bash_profile
-ln -s code/.dotfiles/.zshrc .zshrc
-ln -s code/.dotfiles/.p10k.zsh .p10k.zsh
+ln -s ~/code/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/code/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/code/.dotfiles/.p10k.zsh ~/.p10k.zsh
 
 ln -s ~/code/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/code/.dotfiles/.gitignore_global ~/.gitignore_global
